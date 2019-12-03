73e002c689f77fdfbe98d041c909ba6940abb45a
bkbilly <bkbilly@hotmail.com>
2019-12-03 16:44:27 +0000

    seperate in methods

--- canopy.py
+++ canopy.py
@@ -2,67 +2,123 @@
 
 import cv2
 import numpy as np
+import matplotlib.pyplot as plt
+import copy
 
 
-inputimage = 'realeasy.png'
-cv_image = cv2.imread(inputimage)
-hsv = cv2.cvtColor(cv_image, cv2.COLOR_BGR2HSV)
-gray_image = cv2.cvtColor(cv_image, cv2.COLOR_BGR2GRAY)
-
-img_simple = cv2.imread('images/img_simple.png', cv2.IMREAD_GRAYSCALE)
-img_realeasy = cv2.imread('images/img_realeasy.png', cv2.IMREAD_GRAYSCALE)
-img_realhard = cv2.imread('images/img_realhard.png', cv2.IMREAD_GRAYSCALE)
-
-orb = cv2.ORB()
-kp1, des1 = orb.detectAndCompute(gray_image, None)
-kp2, des2 = orb.detectAndCompute(img_realeasy, None)
-bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
-matches = bf.match(des1, des2)
-matches = sorted(matches, key=lambda x: x.distance)
-print(len(matches))
+def showPlot(img):
+    for i, col in enumerate(['b', 'g', 'r']):
+        hist = cv2.calcHist([img], [i], None, [256], [0, 256])
+        plt.plot(hist, color=col)
+        plt.xlim([0, 256])
+    plt.show()
 
 
-if inputimage == 'images/simple.png':
-    hsv = cv2.blur(hsv, (20, 20))
-    lower_filter = np.array([30, 120, 50])
-    upper_filter = np.array([50, 180, 200])
-elif inputimage == 'images/realeasy.png':
+def filter_colors(cv_image, runtype):
     hsv = cv2.cvtColor(cv_image, cv2.COLOR_BGR2HSV)
-    hsv = cv2.blur(hsv, (40, 40))
-    lower_filter = np.array([30, 100, 60])
-    upper_filter = np.array([60, 170, 130])
-elif inputimage == 'images/realhard.png':
-    hsv = cv2.cvtColor(cv_image, cv2.COLOR_BGR2HSV)
-    hsv = cv2.blur(hsv, (10, 10))
-    lower_filter = np.array([30, 0, 20])
-    upper_filter = np.array([50, 200, 255])
+    if runtype == 'simple':
+        # hsv = cv2.blur(hsv, (10, 10))
+        hsv = cv2.GaussianBlur(hsv, ksize=(17, 17), sigmaX=10)
+        lower_filter = np.array([30, 120, 0])
+        upper_filter = np.array([50, 180, 200])
+    if runtype == 'simple_inv':
+        # hsv = cv2.blur(hsv, (10, 10))
+        hsv = cv2.GaussianBlur(hsv, ksize=(17, 17), sigmaX=10)
+        lower_filter = np.array([0, 0, 0])
+        upper_filter = np.array([255, 80, 255])
+    elif runtype == 'realeasy':
+        hsv = cv2.blur(hsv, (40, 40))
+        hsv = cv2.GaussianBlur(hsv, ksize=(17, 17), sigmaX=10)
+        lower_filter = np.array([0, 10, 40])
+        upper_filter = np.array([60, 150, 255])
+    elif runtype == 'realeasy_inv':
+        # hsv = cv2.blur(hsv, (40, 40))
+        hsv = cv2.GaussianBlur(hsv, ksize=(17, 17), sigmaX=10)
+        lower_filter = np.array([30, 30, 0])
+        upper_filter = np.array([100, 90, 40])
+    elif runtype == 'realhard':
+        hsv = cv2.blur(hsv, (40, 40))
+        # hsv = cv2.GaussianBlur(hsv, ksize=(17,17), sigmaX=10)
+        lower_filter = np.array([40, 40, 0])
+        upper_filter = np.array([100, 80, 200])
+    elif runtype == 'realhard_inv':
+        hsv = cv2.blur(hsv, (40, 40))
+        hsv = cv2.GaussianBlur(hsv, ksize=(17,17), sigmaX=10)
+        lower_filter = np.array([0, 90, 0])
+        upper_filter = np.array([255, 100, 255])
+    elif runtype == 'ground':
+        # hsv = cv2.blur(hsv, (40, 40))
+        hsv = cv2.GaussianBlur(hsv, ksize=(17, 17), sigmaX=10)
+        lower_filter = np.array([0, 30, 30])
+        upper_filter = np.array([20, 140, 80])
+    elif runtype == 'ground_inv':
+        # hsv = cv2.blur(hsv, (40, 40))
+        hsv = cv2.GaussianBlur(hsv, ksize=(17, 17), sigmaX=10)
+        lower_filter = np.array([30, 0, 10])
+        upper_filter = np.array([90, 255, 150])
 
-mask = cv2.inRange(hsv, lower_filter, upper_filter)
-res = cv2.bitwise_and(cv_image, cv_image, mask=mask)
-
-# cv2.imshow('hsv cv_image', hsv)
-# cv2.imshow('res cv_image', res)
-# cv2.waitKey(0)
+    mask = cv2.inRange(hsv, lower_filter, upper_filter)
+    res = cv2.bitwise_and(cv_image, cv_image, mask=mask)
+    # cv2.imshow('hsv', hsv)
+    return res, mask
 
 
-# Grayscale
-gray_res = cv2.cvtColor(res, cv2.COLOR_BGR2GRAY)
-# cv2.imshow('Canny Edges After Contouring', gray_res)
+def get_contours(res, cv_image):
+    # Grayscale
+    gray_res = cv2.cvtColor(res, cv2.COLOR_BGR2GRAY)
+    # cv2.imshow('Canny Edges After Contouring', gray_res)
 
-# Find Canny edges
-# edged = cv2.Canny(gray_res, 150, 150)
-ret, edged = cv2.threshold(gray_res, 40, 255, 0)
+    # Find Canny edges
+    # edged = cv2.Canny(gray_res, 150, 150)
+    ret, edged = cv2.threshold(gray_res, 20, 255, 0)
 
-# Finding Contours
-contours, hierarchy = cv2.findContours(
-    edged,
-    cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-# cv2.imshow('Canny Edges After Contouring', edged)
+    # Finding Contours
+    contours, hierarchy = cv2.findContours(
+        edged,
+        cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    # contours = max(contours, key=cv2.contourArea)
 
-# Draw all contours
-# -1 signifies drawing all contours
-cv2.drawContours(cv_image, contours, -1, (0, 255, 0), 3)
+    threshold_area = 300
+    filtered_contours = []
+    for cnt in contours:
+        area = cv2.contourArea(cnt)
+        if area > threshold_area:
+            filtered_contours.append(cnt)
+    # cv2.imshow('Canny Edges After Contouring', edged)
+    contours = copy.copy(cv_image)
+    cv2.drawContours(contours, filtered_contours, -1, (0, 255, 0), 1)
+    return contours, filtered_contours
 
-cv2.imshow('Contours', cv_image)
+
+inputimage = ['images/plants3/ros_plant3_0.jpg', 'realhard_inv']
+inputimage = ['images/plants3/ros_plant3_1.jpg', 'realhard_inv']
+inputimage = ['images/plants1/ros_plant0.jpg', 'simple_inv']
+inputimage = ['images/plants1/ros_plant0_1.jpg', 'simple_inv']
+inputimage = ['images/plants1/ros_plant0_2.jpg', 'simple_inv']
+# inputimage = ['images/plants2/train/ros_plant2_0.jpg', 'realeasy_inv']
+# inputimage = ['images/plants2/train/ros_plant2_1.jpg', 'realeasy_inv']
+cv_image = cv2.imread(inputimage[0])
+# hsv = cv2.cvtColor(cv_image, cv2.COLOR_BGR2HSV)
+
+# showPlot(cv_image)
+ground, ground_mask = filter_colors(cv_image, 'ground')
+ground_inv, ground_inv_mask = filter_colors(cv_image, 'ground_inv')
+plant, plant_mask = filter_colors(ground_inv, inputimage[1])
+contours, filtered_contours = get_contours(plant, cv_image)
+# contours_s = cv2.resize(contours, (0, 0), fx=0.5, fy=0.5)
+
+# cv2.imshow('Mask', plant)
+plt.subplot(2, 2, 3)
+plt.imshow(plant)
+plt.subplot(2, 2, 2)
+plt.imshow(ground_mask + plant_mask)
+plt.subplot(2, 2, 1)
+plt.imshow(cv2.resize(contours, (0, 0), fx=0.5, fy=0.5))
+# plt.show()
+
+# cv2.imshow('Mask', ground_mask + plant_mask)
+# cv2.imshow('Mask_plant', mask_plant)
+# cv2.imshow('final', contours)
+cv2.imshow('Contours', contours)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
