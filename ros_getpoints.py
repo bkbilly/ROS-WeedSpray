1e9a5a80f49866978ca3a6536fcf51096aaea162
bkbilly <bkbilly@hotmail.com>
2019-12-08 21:23:22 +0000

    working with pointcloud

--- ros_getpoints.py
+++ ros_getpoints.py
@@ -2,14 +2,13 @@
 
 # Python libs
 import sys
+import math
 
 # Ros libraries
 import rospy
-import tf
 
 # Ros Messages
-from geometry_msgs.msg import PointStamped
-from cv_bridge import CvBridge, CvBridgeError
+from sensor_msgs.msg import PointCloud
 
 
 class image_projection():
@@ -19,24 +18,27 @@ class image_projection():
         self.keeplist = []
 
         self.camera_info_sub = rospy.Subscriber(
-            "/weed/point/{}".format(self.robot),
-            PointStamped,
+            "/weed/points/{}".format(self.robot),
+            PointCloud,
             self.points_callback)
 
-
     def points_callback(self, data):
-        new_position = (data.pose.position.x, data.pose.position.y)
-        # print(new_position)
-        found_close = False
-        for keep in self.keeplist:
-            dx = abs(new_position[0] - keep[0])
-            dy = abs(new_position[1] - keep[1])
-            dist = dx + dy
-            if dist < 0.2:
-                found_close = True
+        # print(data)
+        for point in data.points:
+            new_position = (point.x, point.y, point.z)
+            # print(new_position)
+            found_close = False
+            for keep in self.keeplist:
+                dx = abs(new_position[0] - keep[0])
+                dy = abs(new_position[1] - keep[1])
+                # dz = abs(new_position[2] - keep[2])
+                # dist = dx + dy + dz
+                dist = math.hypot(dx, dy)
+                if dist < 0.1:
+                    found_close = True
 
-        if not found_close:
-            self.keeplist.append(new_position)
+            if not found_close:
+                self.keeplist.append(new_position)
         print('Found points: {}'.format(len(self.keeplist)))
         # print(self.keeplist)
 
@@ -44,7 +46,7 @@ class image_projection():
 def main(args):
     '''Initializes and cleanup ros node'''
     rospy.init_node('get_points', anonymous=True)
-    img_proj = image_projection('thorvald_001')
+    image_projection('thorvald_001')
 
     try:
         rospy.spin()
